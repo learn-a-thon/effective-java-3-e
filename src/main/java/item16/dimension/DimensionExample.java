653fe18ce9520d3ddddc319fba0f2c45b1ec1a4f
doorisopen <doorwasopen@gmail.com>
2023-02-26 20:40:12 +0900

    [twlee] 2023-02-26 아이템 16 ~ 18

---
+++ src/main/java/item16/dimension/DimensionExample.java
@@ -0,0 +1,14 @@
+package item16.dimension;
+
+import java.awt.*;
+
+public class DimensionExample {
+    public static void main(String[] args) {
+        Button button = new Button("hello button");
+        button.setBounds(0, 0, 20, 10);
+
+        Dimension size = button.getSize();
+        System.out.println("size.height = " + size.height);
+        System.out.println("size.width = " + size.width);
+    }
+}
