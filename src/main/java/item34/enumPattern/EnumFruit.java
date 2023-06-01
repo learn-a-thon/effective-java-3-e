a0ede21e928621ff189dacd6fb2308bca68bfc83
doorisopen <doorwasopen@gmail.com>
2023-06-01 22:26:31 +0900

    [twlee] 2023-06-01 아이템 34 ~ 36

---
+++ src/main/java/item34/enumPattern/EnumFruit.java
@@ -0,0 +1,11 @@
+package item34.enumPattern;
+
+// 코드 34-2 가장 단순한 열거 타입
+public class EnumFruit {
+    enum Apple {
+        FUJI, PIPPIN, GRANNY_SMITH
+    }
+    enum Orange {
+        NAVEL, TEMPLE, BLOOD
+    }
+}
